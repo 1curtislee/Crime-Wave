e13e8860bc016c1ad22cba42f07061284c4d9003
1curtislee <1curtislee@gmail.com>
2019-03-16 12:53:29 -0700

    0316

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,20 @@ $("#searchBtn").on("click",function(){
 
   var cur1 = $("#cur1").val() /*currency 1 from search input*/;
   var cur2 = $("#cur2").val() /*currency 2 from search input*/;
-
   var date = "2018-03-12" /*date from search input, yyyy-mm-dd format*/;
-    
-  var queryURL ="https://api.exchangeratesapi.io/history?start_at=" + date + "&end_at=" + date + "&base=" + cur1 + "&symbols=" + cur2
+  var queryURL ="https://api.exchangeratesapi.io/history?start_at=" + date + "&end_at=" + date + "&base=" + cur1 + "&symbols=" + cur1 + "," + cur2
 
   $.ajax({
     url:queryURL,
     method: "GET"
   })
     .then(function(response) {
-      console.log(response);
+      console.log(response.rates.2018-03-12.USD);
+      //let exchange = response.rates.date;
+
+
+      $("#ex1").text(response.rates.)
+      $("#ex2").text(response.rates)
 
   })
 })
