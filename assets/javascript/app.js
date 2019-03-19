21222e15e628673e09090159dd8d18ca1db422ae
TJ <thomasjrwinbush@gmail.com>
2019-03-18 19:24:03 -0700

    031819

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
+ 
+
 $("#searchBtn").on("click",function(){
   console.log("search")
 
   var cur1 = $("#cur1").val() /*currency 1 from search input*/;
   var cur2 = $("#cur2").val() /*currency 2 from search input*/;
-  var date = "2018-03-12" /*date from search input, yyyy-mm-dd format*/;
+  var date = $("#date").val() /*date from search input, yyyy-mm-dd format*/;
   var queryURL ="https://api.exchangeratesapi.io/history?start_at=" + date + "&end_at=" + date + "&base=" + cur1 + "&symbols=" + cur1 + "," + cur2
 
   $.ajax({
@@ -12,11 +14,7 @@ $("#searchBtn").on("click",function(){
   })
     .then(function(response) {
       console.log(response);
-      
-
-
-      $("#ex1").text(response.rates[date][cur1]);
-      $("#ex2").text(response.rates[date][cur2]);
-
+      $("#ex1").text(cur1 + ": " + response.rates[date][cur1])
+      $("#ex2").text(cur2 + ": " + response.rates[date][cur2])
   })
 })
