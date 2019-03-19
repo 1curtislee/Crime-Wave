a4fb40ba46587dd866ebd209cb1dac9c4e55adf4
TJ <thomasjrwinbush@gmail.com>
2019-03-18 21:25:43 -0700

    031819

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,22 @@
- 
+var myStorage = window.localStorage;
+
+var getEx1 = localStorage.getItem('cur1');
+var getEx2 = localStorage.getItem('cur2');
+
+function setUp() {
+  $("#ex1").text(getEx1);
+  $("#ex2").text(getEx2);
+}
 
 $("#searchBtn").on("click",function(){
   console.log("search")
-
+  
+  
   var cur1 = $("#cur1").val() /*currency 1 from search input*/;
   var cur2 = $("#cur2").val() /*currency 2 from search input*/;
   var date = $("#date").val() /*date from search input, yyyy-mm-dd format*/;
-  var queryURL ="https://api.exchangeratesapi.io/history?start_at=" + date + "&end_at=" + date + "&base=" + cur1 + "&symbols=" + cur1 + "," + cur2
+  var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
+  
 
   $.ajax({
     url:queryURL,
@@ -14,7 +24,9 @@ $("#searchBtn").on("click",function(){
   })
     .then(function(response) {
       console.log(response);
-      $("#ex1").text(cur1 + ": " + response.rates[date][cur1])
-      $("#ex2").text(cur2 + ": " + response.rates[date][cur2])
+      $("#ex1").text(cur1 + ": " + response.rates[cur1])
+      $("#ex2").text(cur2 + ": " + response.rates[cur2])
+      localStorage.setItem("cur1",JSON.stringify(response.rates[cur1]));
+      localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
   })
 })
