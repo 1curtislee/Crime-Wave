1534d1d518fc181c4c1bc916c58ba7ad5e617328
1curtislee <1curtislee@gmail.com>
2019-03-18 19:08:58 -0700

    testing git

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $("#searchBtn").on("click",function(){
 
   var cur1 = $("#cur1").val() /*currency 1 from search input*/;
   var cur2 = $("#cur2").val() /*currency 2 from search input*/;
-  var date = "2018-03-12" /*date from search input, yyyy-mm-dd format*/;
+  var date = $("#date").val() /*date from search input, yyyy-mm-dd format*/;
   var queryURL ="https://api.exchangeratesapi.io/history?start_at=" + date + "&end_at=" + date + "&base=" + cur1 + "&symbols=" + cur1 + "," + cur2
 
   $.ajax({
