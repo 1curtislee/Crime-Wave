e6f5714b511bc71e2582bb2fd00662d615b424ec
1curtislee <1curtislee@gmail.com>
2019-03-20 21:27:22 -0700

    0320

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,48 @@
 var myStorage = window.localStorage;
+
+var getEx1 = localStorage.getItem('cur1');
+var getEx2 = localStorage.getItem('cur2');
+var getCur1Lable = localStorage.getItem('cur1Lable');
+var getCur2Lable = localStorage.getItem('cur2Lable');
 function onBodyLoad(){
-  $("#ex1").text(getEx1);
-  $("#ex2").text(getEx2);
-  var getEx1 = localStorage.getItem('cur1');
-  var getEx2 = localStorage.getItem('cur2');
-  
+  $("#ex2").text(getCur2Lable + ": " + getEx2);
+  $("#ex1").text(getCur1Lable + ": " + getEx1);
+
+
   const time = document.getElementById('date');
   const now = moment();
   const readable = now.format('Y-MM-D');
-  time.placeholder = readable;
+  console.log(readable);
+  console.log(time.placeholder);
+  time.placeholder = readable;  
 }
-$("#searchBtn").on("click",function(){
-  var cur1 = $("#cur1").val();
-  var cur2 = $("#cur2").val();
-  var date = $("#date").val().trim();
-  if (date === ''){
-    date = moment().format('Y-MM-D')
-  }
-  var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
-  $.ajax({
-    url:queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      console.log(response);
-      $("#ex1").text(cur1 + ": " + response.rates[cur1])
-      $("#ex2").text(cur2 + ": " + response.rates[cur2])
-      localStorage.setItem("cur1",JSON.stringify(response.rates[cur1]));
-      localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
-  })
-})
+
+  $("#searchBtn").on("click",function(){
+    console.log("search")
+
+    var cur1 = $("#cur1").val(); /*currency 1 from search input*/
+    var cur2 = $("#cur2").val(); /*currency 2 from search input*/
+    var date = $("#date").val().trim(); /*date from search input, yyyy-mm-dd format*/
+
+    if (date === ''){
+      date = moment().format('Y-MM-D')
+    }
+    var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
+
+    $.ajax({
+      url:queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        console.log(response);
+        $("#ex1").text(cur1 + ": " + response.rates[cur1])
+        $("#ex2").text(cur2 + ": " + response.rates[cur2])
+        
+        localStorage.setItem("cur1",JSON.stringify(response.rates[cur1]));
+        localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
+        localStorage.setItem("cur1Lable", JSON.stringify(cur1));
+        localStorage.setItem("cur2Lable", JSON.stringify(cur2));
+      })
+
+    })
+  
