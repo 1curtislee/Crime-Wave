36cb71933110eddf26fe46ff9b2137159ead5937
1curtislee <1curtislee@gmail.com>
2019-03-20 21:00:22 -0700

    date fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,28 @@
 var myStorage = window.localStorage;
-
-var getEx1 = localStorage.getItem('cur1');
-var getEx2 = localStorage.getItem('cur2');
-
 function onBodyLoad(){
-  $("#ex1").text(cur1 + ": " + getEx1);
-  $("#ex2").text(cur2 + ": " + getEx2);
+  $("#ex1").text(getEx1);
+  $("#ex2").text(getEx2);
+  var getEx1 = localStorage.getItem('cur1');
+  var getEx2 = localStorage.getItem('cur2');
   
   const time = document.getElementById('date');
   const now = moment();
   const readable = now.format('Y-MM-D');
-
-  //console.log(readable);
-  //console.log(time.placeholder);
-
   time.placeholder = readable;
 }
-
 $("#searchBtn").on("click",function(){
-  var cur1 = $("#cur1").val(); /*currency 1 from search input*/
-  var cur2 = $("#cur2").val(); /*currency 2 from search input*/
-  var date = $("#date").val().trim(); /*date from search input, yyyy-mm-dd format*/
+  var cur1 = $("#cur1").val();
+  var cur2 = $("#cur2").val();
+  var date = $("#date").val().trim();
+  if (date === ''){
+    date = moment().format('Y-MM-D')
+  }
   var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
-
   $.ajax({
     url:queryURL,
     method: "GET"
   })
     .then(function(response) {
-
       console.log(response);
       $("#ex1").text(cur1 + ": " + response.rates[cur1])
       $("#ex2").text(cur2 + ": " + response.rates[cur2])
@@ -36,4 +30,3 @@ $("#searchBtn").on("click",function(){
       localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
   })
 })
-
