39279596cad2765a479bd251e8db9fe7956d6780
1curtislee <1curtislee@gmail.com>
2019-08-10 13:25:41 -0700

    remove console logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-var myStorage = window.localStorage;
-
 var getEx1 = localStorage.getItem('cur1');
 var getEx2 = localStorage.getItem('cur2');
 var getCur1Lable = localStorage.getItem('cur1Lable');
@@ -12,37 +10,32 @@ function onBodyLoad(){
   const time = document.getElementById('date');
   const now = moment();
   const readable = now.format('Y-MM-D');
-  console.log(readable);
-  console.log(time.placeholder);
   time.placeholder = readable;  
 }
 
-  $("#searchBtn").on("click",function(){
-    console.log("search")
+$("#searchBtn").on("click",function(){
+  var cur1 = $("#cur1").val(); /*currency 1 from search input*/
+  var cur2 = $("#cur2").val(); /*currency 2 from search input*/
+  var date = $("#date").val().trim(); /*date from search input, yyyy-mm-dd format*/
 
-    var cur1 = $("#cur1").val(); /*currency 1 from search input*/
-    var cur2 = $("#cur2").val(); /*currency 2 from search input*/
-    var date = $("#date").val().trim(); /*date from search input, yyyy-mm-dd format*/
+  if (date === ''){
+    date = moment().format('Y-MM-D')
+  }
+  var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
 
-    if (date === ''){
-      date = moment().format('Y-MM-D')
-    }
-    var queryURL = "https://api.exchangeratesapi.io/" + date + "?base=" + cur1 + "&symbols=" + cur1 + "," + cur2;
-
-    $.ajax({
-      url:queryURL,
-      method: "GET"
+  $.ajax({
+    url:queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      $("#ex1").text(cur1 + ": " + response.rates[cur1])
+      $("#ex2").text(cur2 + ": " + response.rates[cur2])
+      
+      localStorage.setItem("cur1",JSON.stringify(response.rates[cur1]));
+      localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
+      localStorage.setItem("cur1Lable", JSON.stringify(cur1));
+      localStorage.setItem("cur2Lable", JSON.stringify(cur2));
     })
-      .then(function(response) {
-        console.log(response);
-        $("#ex1").text(cur1 + ": " + response.rates[cur1])
-        $("#ex2").text(cur2 + ": " + response.rates[cur2])
-        
-        localStorage.setItem("cur1",JSON.stringify(response.rates[cur1]));
-        localStorage.setItem("cur2",JSON.stringify(response.rates[cur2]));
-        localStorage.setItem("cur1Lable", JSON.stringify(cur1));
-        localStorage.setItem("cur2Lable", JSON.stringify(cur2));
-      })
 
-    })
+  })
   
